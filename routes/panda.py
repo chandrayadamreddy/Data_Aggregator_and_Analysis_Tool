f88d2ee1abe5574d90aba44864dd8df8228c6a90
chand <chanduy4all@gmail.com>
2024-04-30 11:00:48 -0500

    added a route to upload a function

--- routes/panda.py
+++ routes/panda.py
@@ -1,5 +1,29 @@
-from flask import Blueprint
+from urllib import request
 
+from flask import Blueprint
+from werkzeug.utils import secure_filename
 realtime = Blueprint("realtime", __name__)
-def PANDA_LIB():
-    return None
+
+ALLOWED_EXTENSIONS = {'json', 'txt', 'xlsx'}
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@realtime.route("/upload", methods=['POST'])
+
+def upload_file():
+    try:
+        file = request.files.get("data")
+        if file is None:
+            return "No file uploaded", 400
+
+        if file and allowed_file(file.filename):
+            # Proceed with file processing
+            filename = secure_filename(file.filename)
+            # Here you can handle uploading to MongoDB or any other operations
+            return "Data Uploaded successfully to MongoDB!!!", 200
+        else:
+            return "Invalid file type. Allowed file types are: JSON, TXT, XLSX.", 400
+    except Exception as e:
+        print(e)
+        return "An error occurred while processing the file", 500
